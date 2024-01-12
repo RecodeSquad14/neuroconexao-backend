768e82c386e6d8fe98660788c292d7885f1df842
Anderson1605R <114451515+Anderson1605R@users.noreply.github.com>
2024-01-11 22:53:01 -0300

    setup do spring

---
+++ src/test/java/br/com/neuroconexao/neuroconexaobackend/NeuroconexaobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.neuroconexao.neuroconexaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroconexaobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
