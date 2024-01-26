dbd4c783f4d96524919d69d64fa13cdd0917262e
Marcelle Santos <marcelledossantos2@gmail.com>
2024-01-26 00:42:43 -0300

    refactor

--- src/main/java/br/com/neuroconexao/neuroconexaobackend/config/SecurityConfig.java
+++ src/main/java/br/com/neuroconexao/neuroconexaobackend/config/SecurityConfig.java
@@ -1,24 +1,24 @@
 package br.com.neuroconexao.neuroconexaobackend.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebSecurity
+@EnableMethodSecurity
+@EnableWebMvc
 @Configuration
 public class SecurityConfig {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -27,19 +27,19 @@ public class SecurityConfig {
    @Bean
    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
        http.csrf(csrf -> csrf.disable())
-               .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/usuarios")
-                       .permitAll().requestMatchers("/neurodiversos", "/api/profissionais", "/empresas").hasRole("ADMIN"))
-               .formLogin(form -> form.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/")
-                       .permitAll())
-               .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll());
-
+               .authorizeHttpRequests(auth -> auth
+                       .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                       .requestMatchers(HttpMethod.POST, "/api/v1/auth").permitAll()
+                       .anyRequest().authenticated());
        return http.build();
    }
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
+
 
 
 
--- src/main/java/br/com/neuroconexao/neuroconexaobackend/controllers/UsuarioController.java
+++ src/main/java/br/com/neuroconexao/neuroconexaobackend/controllers/UsuarioController.java
@@ -31,7 +31,7 @@ public class UsuarioController {
         }
     }
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN') OR (hasRole('CLIENTE') AND #id == authentication.principal.id)")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Usuario> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(user);
